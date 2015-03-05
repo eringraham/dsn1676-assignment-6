a6dd1e72fee9b9bc00b4a2aa43e1da8616d750f7
eringraham <eringraham@Erins-MacBook-Pro.local>
2015-03-05 16:02:25 -0500

    made ball bounce in

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var $diamond = $('.diamond');
 var $btnCollapseExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle')
+
 $btnShowHide.on('click', function() {
     $box.toggleClass('js-show-hide');
 });
@@ -17,4 +20,12 @@ $btnMove.on('click', function() {
 
 $btnCollapseExpand.on('click', function() {
     $panel.toggleClass('js-panel');
+});
+
+$btnBounce.on('click', function() {
+    $circle.addClass('js-bounce');
+});
+
+$circle.on('webkitAnimationEnd animationend', function () {
+    $circle.removeClass('js-bounce');
 });
